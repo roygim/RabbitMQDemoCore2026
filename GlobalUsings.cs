cbfdc6f07b8b31bf5789984a66bec678bb161cd8
Roei <roygim@gmail.com>
2026-06-30 15:58:28 +0300

    roeig

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,3 @@
-﻿global using RabbitMQDemoCore2026.Entities;
+﻿global using RabbitMQDemoCore2026.Domain.Entities;
 global using RabbitMQDemoCore2026.Interfaces;
 global using RabbitMQDemoCore2026.Repositories;
